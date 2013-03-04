e2a49c8e92108922c60cd503399e76423a7173fa
Matt Campbell <matthewricecampbell@gmail.com>
2013-03-03 19:21:08 -0600

    deleted out of date migrations

--- db/migrate/20130302200919_change_stats_table_names.rb
+++
@@ -1,9 +0,0 @@
-class ChangeStatsTableNames < ActiveRecord::Migration
-  def self.up
-    rename_table :player_batting_stats, :stats
-  end
-
- def self.down
-    rename_table :stats, :player_batting_stats
- end
-end
